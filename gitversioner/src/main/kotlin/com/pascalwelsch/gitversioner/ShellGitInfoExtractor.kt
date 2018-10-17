3cee69a55485578ca341d835c06eb833f4eb1cea
Pascal Welsch <pascal.welsch@gmail.com>
2018-10-18 01:25:37 +0200

    Read process stdout correctly via project.exec
    Prevents hang during `git status` for large output

--- gitversioner/src/main/kotlin/com/pascalwelsch/gitversioner/ShellGitInfoExtractor.kt
+++ gitversioner/src/main/kotlin/com/pascalwelsch/gitversioner/ShellGitInfoExtractor.kt
@@ -1,8 +1,8 @@
 package com.pascalwelsch.gitversioner
 
-import org.codehaus.groovy.runtime.ProcessGroovyMethods
+import org.gradle.api.GradleException
 import org.gradle.api.Project
-import java.util.Collections.emptyList
+import java.io.ByteArrayOutputStream
 
 interface GitInfoExtractor {
     val currentSha1: String?
@@ -22,19 +22,19 @@ internal class ShellGitInfoExtractor(val project: Project) : GitInfoExtractor {
 
     override val currentSha1: String? by lazy {
         if (!isGitProjectReady) return@lazy null
-        val sha1 = "git rev-parse HEAD".execute().text().trim()
+        val sha1 = "git rev-parse HEAD".execute().throwOnError().text.trim()
         if (sha1.isEmpty()) null else sha1
     }
 
     override val currentBranch: String? by lazy {
         if (!isGitProjectReady) return@lazy null
-        val branch = "git symbolic-ref --short -q HEAD".execute().text().trim()
+        val branch = "git symbolic-ref --short -q HEAD".execute().throwOnError().text.trim()
         if (branch.isEmpty()) null else branch
     }
 
     override val localChanges: LocalChanges by lazy {
         if (!isGitProjectReady) return@lazy NO_CHANGES
-        val shortStat = "git diff HEAD --shortstat".execute().text().trim()
+        val shortStat = "git diff HEAD --shortstat".execute().throwOnError().text.trim()
         if (shortStat.isEmpty()) return@lazy NO_CHANGES
 
         return@lazy parseShortStats(shortStat)
@@ -42,15 +42,15 @@ internal class ShellGitInfoExtractor(val project: Project) : GitInfoExtractor {
 
     override val initialCommitDate: Long by lazy {
         val initialCommit: String = commitsToHead.lastOrNull() ?: return@lazy 0L
-        val time = "git log $initialCommit -n 1 --pretty=format:'%at'".execute()
-                .text().replace("\'", "").trim()
+        val time = listOf("git", "log", "-n 1", "--pretty=format:'%at'", initialCommit).execute()
+                .throwOnError().text.replace("\'", "").trim()
 
         return@lazy if (time.isEmpty()) 0L else time.toLong()
     }
 
     override fun commitDate(rev: String): Long {
-        val time = "git log $rev --pretty=format:'%at' -n 1".execute()
-                .text().replace("\'", "").trim()
+        val time = listOf("git", "log", "--pretty=format:'%at'", "-n 1", rev).execute()
+                .throwOnError().text.replace("\'", "").trim()
         return if (time.isEmpty()) 0 else time.toLong()
     }
 
@@ -58,57 +58,80 @@ internal class ShellGitInfoExtractor(val project: Project) : GitInfoExtractor {
 
     override val isGitProjectReady: Boolean by lazy {
         val result = "git status".execute()
-
-        when (result.exitValue()) {
-            0 -> true
-            69 -> {
-                println("git returned with error 69\n" +
-                        "If you are a mac user that message is telling you is that you need to open the " +
-                        "application XCode on your Mac OS X/macOS and since it hasn’t run since the last " +
-                        "update, you need to accept the new license EULA agreement that’s part of the " +
-                        "updated XCode.\n\n" +
-                        "tl;dr run\n" +
-                        "\txcode-select --install")
-                false
-            }
-            else -> {
-                println("ERROR: can't generate a git version, this is not a git project")
-                println(" -> Not a git repository (or any of the parent directories): .git")
-                false
+        when (result) {
+            is ProcessResult.Success -> true
+            is ProcessResult.Error -> {
+                val exitCode = result.errorCode
+                when (exitCode) {
+                    0 -> true
+                    69 -> {
+                        println("git returned with error 69\n" +
+                                "If you are a mac user that message is telling you is that you need to open the " +
+                                "application XCode on your Mac OS X/macOS and since it hasn’t run since the last " +
+                                "update, you need to accept the new license EULA agreement that’s part of the " +
+                                "updated XCode.\n\n" +
+                                "tl;dr run\n" +
+                                "\txcode-select --install")
+                        false
+                    }
+                    else -> {
+                        println("ERROR: can't generate a git version, this is not a git project. git status errors with error code: $exitCode")
+                        println(" -> Not a git repository (or any of the parent directories): .git")
+                        false
+                    }
+                }
             }
         }
     }
 
     override fun commitsUpTo(rev: String, args: String): List<String> {
-        val cmd = "git rev-list $rev $args"
 
-        var lines: List<String>
-        var process = cmd.execute(waitFor = false)
-        try {
-            lines = process.inputStream.bufferedReader().readLines()
+        val text = try {
+            "git rev-list $rev $args".execute().throwOnError().text
         } catch (e: Exception) {
             try {
-                process = "git rev-list origin/$rev $args".execute()
-                lines = process.inputStream.bufferedReader().readLines()
+                "git rev-list origin/$rev $args".execute().throwOnError().text
             } catch (e: Exception) {
-                lines = emptyList()
+                ""
             }
         }
 
-        process.destroy()
-
-        val list = lines.map { it.trim() }
-        return list
+        return text.lines().asSequence().map { it.trim() }.filter { it.isNotBlank() }.toList()
     }
 
-    private fun Process.text(): String = ProcessGroovyMethods.getText(this)
+    private fun String.execute(): ProcessResult = split(" ").execute()
 
-    private fun String.execute(waitFor: Boolean = true): Process {
-        val process = ProcessGroovyMethods.execute(this, emptyArray<String>(), project.projectDir)
-        if (waitFor) {
-            process.waitFor()
+    private fun List<String>.execute(): ProcessResult {
+        val out = ByteArrayOutputStream()
+        val err = ByteArrayOutputStream()
+        val task = project.exec {
+            it.commandLine = this@execute
+            it.standardOutput = out
+            it.errorOutput = err
+            it.workingDir = project.projectDir
+            it.isIgnoreExitValue = true
         }
-        return process
+        val exitCode = task.exitValue
+        return if (exitCode == 0) {
+            ProcessResult.Success(out.toString())
+        } else {
+            ProcessResult.Error(err.toString(), exitCode, this.joinToString(" "))
+        }
+    }
+
+
+    private sealed class ProcessResult {
+        class Success(val text: String) : ProcessResult()
+        class Error(val text: String, val errorCode: Int, val command: String) : ProcessResult() {
+            fun errorMessage(): String = "Error $errorCode executing `$command`\n\n${text.take(100)}"
+        }
+
+        fun throwOnError(): Success {
+            when (this) {
+                is Success -> return this
+                is Error -> throw GradleException(this.errorMessage())
+            }
+        }
     }
 }
 
